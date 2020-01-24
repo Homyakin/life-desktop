78f019ad3590ccc70f717b70edac83804600e523
Homyakin <tox.home@ya.ru>
2020-01-24 20:32:04 +0300

    mouse button + random

--- main.cpp
+++ main.cpp
@@ -1,17 +1,33 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <ctime>
+#include <random>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
+    std::mt19937_64 gen(std::time(nullptr));
+    std::uniform_int_distribution color_dist(0, 255);
+    sf::RenderWindow window(
+            sf::VideoMode(512, 512),
+            "SFML works!",
+            sf::Style::Titlebar | sf::Style::Close
+    );
+    sf::CircleShape shape(256.f);
+    shape.setFillColor(sf::Color::Red);
+    sf::Clock clock{};
+    while (window.isOpen()) {
+        std::cout << clock.getElapsedTime().asSeconds() << std::endl;
+        sf::Event event{};
+        while (window.pollEvent(event)) {
+            switch (event.type) {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                case sf::Event::MouseButtonPressed:
+                    shape.setFillColor(sf::Color(color_dist(gen), color_dist(gen), color_dist(gen)));
+                    break;
+                default:
+                    break;
+            }
         }
 
         window.clear();
